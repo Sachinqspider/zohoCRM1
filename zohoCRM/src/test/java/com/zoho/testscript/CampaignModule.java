99c8e8444d10e35cad5b8a21eb2b28541c5a186f
Reneuit <Reneuit@DESKTOP-MMOB49H>
2024-09-12 18:41:11 +0530

    zoloCRM

---
+++ zohoCRM/src/test/java/com/zoho/testscript/CampaignModule.java
@@ -0,0 +1,13 @@
+package com.zoho.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CampaignModule {
+	@Test
+	public void CreateCustomer() {
+		Reporter.log("CreateCustomer",true);
+		
+	}
+
+}
